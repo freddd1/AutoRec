45df9365959af72ece9c9e00541e1ac03078cab9
ronbo <ronboxer17@gmail.com>
2022-02-07 20:03:54 +0200

    Modify 'mf_train' function in 'matrixfactorization.py' to use batch size with Dataloaders.

--- src/matrixfactorization.py
+++ src/matrixfactorization.py
@@ -1,7 +1,10 @@
 from torch import nn
 import torch
+from torch.utils.data import DataLoader
+import numpy as np
 
 # TODO: Create local(maybe global) 'device' variable.
+
 
 class MatrixFactorization(nn.Module):
     """
@@ -30,36 +33,56 @@ class MatrixFactorization(nn.Module):
         return (user*item).sum(axis=1)
 
 
-def mf_train(train, model, device, epochs=10, lr=0.001, reg=0):
+def mf_train(train, model, device, batch_size=256, epochs=10, lr=0.001, reg=0):
     # TODO:
     #  1.Create variable that calculates the validation loss during the training.
-    #  2.Modify the function to use batch size.(after dataloader class ready)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=reg)
+    dataloader = DataLoader(train.values, batch_size=batch_size)
+    best_loss = 5
     for i in range(epochs):
-        model.train()
-        # users and items indexes start 1, therefore we use the -1
-        users = torch.LongTensor(train.user_id.values-1).to(device)
-        items = torch.LongTensor(train.item_id.values-1).to(device)
-        ratings = torch.FloatTensor(train.rating.values).to(device) # rating is our label
+        for batch_id, train_batch in enumerate(dataloader):
+            # model.train()
+            users = tensor_to_numpy_column(train_batch,0)
+            items = tensor_to_numpy_column(train_batch, 1)
+            ratings = tensor_to_numpy_column(train_batch, 2)
 
-        preds = model(users, items)
-        loss = torch.sqrt(nn.functional.mse_loss(preds, ratings))
+            users = torch.LongTensor(users).to(device)
+            items = torch.LongTensor(items).to(device)
+            ratings = torch.FloatTensor(ratings).to(device) # rating is our label
 
-        # backpropagation
-        optimizer.zero_grad()
-        loss.backward()
+            preds = model(users, items)
+            loss = torch.sqrt(nn.functional.mse_loss(preds, ratings))
 
-        # update
-        optimizer.step()
+            # backpropagation
+            optimizer.zero_grad()
+            loss.backward()
 
-        print(f'train RMSE: {loss.item()}')
+            # update
+            optimizer.step()
+
+            print(f'Train RMSE: {loss.item()}')
+
+            # Save Best Loss score
+            if best_loss> loss.data:
+                best_loss = loss.data
+
+        print('#####################################')
+        print(f'\n Epoch Number: {i+1} \n')
+        print(f'\n Currently Best loss : {best_loss} \n')
+        print('#####################################')
 
 
-#Data loader
-def mf_dataloader():
-    '''
-    '''
-    # TODO:
-    #  Build data loader function that returns dataloader class from pytorch. just like in autorec class.
-    pass
+def tensor_to_numpy_column(batch: DataLoader, column_num: int) -> np.array:
+    """
+    Helper Function to get the relevant columns (users, items, ratings)
+    :param batch: tensor in the shape of Batch_size, holds matrix of (users, items, ratings)
+    :param column_num: column index.
+    :return: Numpy array with relevant column(users, items, ratings).
+    """
+    column = list(map(lambda batch: list(map(lambda z: z.tolist() - 1, batch)), batch))
+    column = np.array(column)[:, column_num]
+    column = list(map(lambda z: torch.tensor(z), column))
+    return column
+
+
 
