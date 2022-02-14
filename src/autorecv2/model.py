c3bf00f1dd62edbf2893e78a81d5ec77220d6012
freddd1 <itamar.fradkin@gmail.com>
2022-02-14 18:30:36 +0200

    added `autorecv2` to src with all the needed files

---
+++ src/autorecv2/model.py
@@ -0,0 +1,29 @@
+from torch import nn
+class AutoRecV2(nn.Module):
+    """
+    AutoRec model.
+    """
+    def __init__(self, num_features, num_hidden=500):
+        """
+        :param num_hidden: Size of the hidden layer
+        """
+        super().__init__()
+        self.num_hidden = num_hidden
+
+        self.encoder = nn.Sequential(
+            nn.Linear(num_features, num_hidden),
+            nn.Sigmoid()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(num_hidden, num_features),
+            nn.Identity()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def params(self):
+        return {'num_hidden': self.num_hidden}
